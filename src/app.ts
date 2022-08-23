15253ec2fa0dca91a5e92fe4910451124c694330
walacenascimento <wlacens@hotmail.com>
2022-08-23 15:58:46 -0300

    implementa a rota e importa no app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
+
+// import carRouter from './routers/index';
 
 const app = express();
+app.use(express.json());
+// app.use(carRouter);
 
 export default app;
---
+++ src/routers/index.ts
@@ -0,0 +1,11 @@
+// import { Router } from 'express';
+
+// const router = Router();
+
+// const carsModel = new CarsModel();
+// const carsService = new CarsService(carsModel);
+// const carsController = new CarsController(carsService);
+
+// router.post('/cars', (_req, _res) => carsController.create(_req, _res));
+
+// export default router;
