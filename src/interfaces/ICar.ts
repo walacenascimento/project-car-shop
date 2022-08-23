16789eca59907a3b02f24351d4bd6854aef8c3e7
walacenascimento <wlacens@hotmail.com>
2022-08-23 14:32:11 -0300

    Implementa a interface ICar, resolve o requisito 3

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { vehicleZodSchema } from './IVehicle';
+
+const carZodSchema = vehicleZodSchema.extend({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+});
+
+type ICar = z.infer<typeof carZodSchema>;
+
+export { carZodSchema, ICar };
