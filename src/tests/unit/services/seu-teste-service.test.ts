cbf4807201c1d9a11894805b57083a48b405dd0c
walacenascimento <wlacens@hotmail.com>
2022-08-24 12:02:01 -0300

    Implementando testes da camdada de service, resolve o requisito 6

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,29 +1,31 @@
 // template para criação dos testes de cobertura da camada de service
 
+import * as sinon from 'sinon';
+// import chai from 'chai';
+import { expect } from 'chai';
+import CarsModel from '../../../models/CarsModel';
+import CarsService from '../../../services/CarsService';
+import { carMock, carMockWithId } from '../../mock/carMock'
+import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog'
 
-// import * as sinon from 'sinon';
-// // import chai from 'chai';
-// import { expect } from 'chai';
-// import carsModel from '../../../models/CarsModel';
-// import carsService from '../../../services/carsService';
-// import { carMock, carMockWithId } from '../../mock/carMock'
-// import { ZodError } from 'zod';
-// import { ErrorTypes } from '../../../errors/catalog'
+describe('Test Car Service', () => {
+    const carsModel = new CarsModel();
+    const carsService = new CarsService(carsModel);
 
-// describe('Test Car Service', () => {
-//     const carsModel = new CarsModel();
-//     const carsService = new CarsService(carsModel);
+  before(async () => {
+    sinon.stub(carsModel, 'create').resolves(carMockWithId);
+  });
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  after(()=>{
+    sinon.restore();
+  })
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  describe('Create a car service', () => {
+      it('Successfully created', async () => {
+        const newCar =  await carsService.create(carMock);
+        expect(newCar).to.be.deep.equal(carMockWithId);
+      });
+  })
 
-//   it('', async () => {});
-
-// });
+});
